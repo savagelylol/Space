d996173f5515c60667a49679b28b5f93897e2ca1
savagelylol <aycenchavez@gpacharter.org>
2025-10-30 16:53:58 -0600

    Update site URL in astro.config.mjs

--- frontend/astro.config.mjs
+++ frontend/astro.config.mjs
@@ -5,7 +5,7 @@ import tailwind from "@astrojs/tailwind";
 import vercel from "@astrojs/vercel";
 
 export default defineConfig({
-  site: "https://your-production-domain.vercel.app",
+  site: "https://space-proxy-tau.vercel.app",
   base: "/",
   integrations: [
     tailwind()
